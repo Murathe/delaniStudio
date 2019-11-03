f055acccfb23b215bee710036894c2d21d772d54
murathe <alicemwihaki99@gmail.com>
2019-11-03 19:29:19 +0300

    Add form validation function

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+// $(document).ready(html)function({
+//     $("");
+// })
+
+function onclick() {
+    const name =  document.getElementById("name");
+    const email =  document.getElementById("email");
+
+
+    if (name == "" || email = "") {
+        alert("Please enter your details");
+    } else {
+        alert("Thank you for contacting unescape, we'll be intouch.");
+    }
+}
