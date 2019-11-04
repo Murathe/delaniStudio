0d3b221a8e90290cb4b56a71336d9f2152a459bc
murathe <alicemwihaki99@gmail.com>
2019-11-04 10:10:43 +0300

    Query selector init

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,29 @@
 $(document).ready(function(){
-    $(".to-toggle p").hide();
-    $("#design").click(function(){
-        $("#design p").toggle();
-        $("#design img").toggle();
+    $("#img-p0").click(function(){
+        $("#design").show(2000);
+        $("#d_image").hide(2000);
     });
-    $("#dev").click(function(){
-        $("#dev p").toggle();
-        $("#dev img").toggle();
+    $("design-p").click(function(){
+        $('#d_image').show(2000);
+        $('#design').hide(2000);
     });
-    $("#prod").click(function(){
-        $("#prod p").toggle();
-        $("#prod img").toggle();
+
+    $("#img-p1").click(function(){
+        $('#dev').show(2000);
+        $('#de_image').hide(2000);
+    });
+    $("#dev-p").click(function(){
+        $('#de_image').show(2000);
+        $('#dev').hide(2000);
+    });
+
+    $("#img-p2").click(function(){
+        $('#prod').show(2000);
+        $('#p_image').hide(2000);
+    });
+    $("#prod-p").click(function(){
+        $('#p_image').show(2000);
+        $('#prod').hide(2000);
     });
 });
+
